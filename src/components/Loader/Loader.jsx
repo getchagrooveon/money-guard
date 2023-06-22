baa3092c0477fd91f948be480c66d5e17068fb85
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-06-22 18:47:01 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { ThreeCircles } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
     <div>
-      <span class="loader"></span>
+      <ThreeCircles
+        height="100"
+        width="100"
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor="#9E40BA"
+        innerCircleColor=" #FFC727"
+        middleCircleColor="#7000FF"
+      />
     </div>
   );
 };
