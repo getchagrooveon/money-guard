18bd6de59db73618439ffb033db1d68bc97b0558
Iryna <irinkasatanovskaya@gmail.com>
2023-06-23 17:58:10 +0300

    add modal log out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import css from './Header.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from 'redux/auth/operations';
 import { useState } from 'react';
+import { ModalLogOut } from 'components/ModalLogOut/ModalLogOut';
 
 export const Header = () => {
   const [modal, setModal] = useState(false);
@@ -14,6 +15,7 @@ export const Header = () => {
 
   const handleLogOut = () => {
     dispatch(logoutUser());
+    console.log('logOut');
   };
 
   const openModal = () => {
@@ -23,24 +25,28 @@ export const Header = () => {
 
   const closeModal = () => {
     setModal(false);
+    console.log('modal log out close');
   };
 
   return (
-    <header className={css.header}>
-      <div className="container">
-        <NavLink to="/" className={css.logo}>
-          <LogoSVG className={css.logoSvg} />
-          <span className={css.logoText}>Money Guard</span>
-        </NavLink>
-        <div className={css.headerLeft}>
-          <p className={css.name}>{userName}</p>
-          <div className={css.verticalLine}></div>
-          <button type="button" className={css.btnLogOut} onClick={openModal}>
-            <IconExit className={css.iconExit} />
-            <p className={css.hiddenExit}>Exit</p>
-          </button>
+    <>
+      <header className={css.header}>
+        <div className="container">
+          <NavLink to="/" className={css.logo}>
+            <LogoSVG className={css.logoSvg} />
+            <span className={css.logoText}>Money Guard</span>
+          </NavLink>
+          <div className={css.headerLeft}>
+            <p className={css.name}>{userName}</p>
+            <div className={css.verticalLine}></div>
+            <button type="button" className={css.btnLogOut} onClick={openModal}>
+              <IconExit className={css.iconExit} />
+              <p className={css.hiddenExit}>Exit</p>
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      {modal && <ModalLogOut closeModal={closeModal} logout={handleLogOut} />}
+    </>
   );
 };
