7fedaac2b95a88da73bdb859e32531b075ded68c
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-06-23 15:14:52 +0300

    auth check

--- src/components/PrivateRoutes/PrivateRoutes.jsx
+++ src/components/PrivateRoutes/PrivateRoutes.jsx
@@ -1,6 +1,10 @@
 import NavBar from 'components/NavBar/NavBar';
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+<<<<<<< Updated upstream
+=======
+import { selectIsLoggedIn } from 'redux/auth/selectors';
+>>>>>>> Stashed changes
 
 const isAuth = true;
 const PrivateRoutes = () => {
--- src/components/PublicRoutes/PublicRoutes.jsx
+++ src/components/PublicRoutes/PublicRoutes.jsx
@@ -3,6 +3,11 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const isAuth = false;
 const PublicRoutes = () => {
+<<<<<<< Updated upstream
+=======
+  const isAuth = useSelector(selectIsLoggedIn);
+
+>>>>>>> Stashed changes
   return !isAuth ? <div>{<Outlet />}</div> : <Navigate to="/dashboard" />;
 };
 
