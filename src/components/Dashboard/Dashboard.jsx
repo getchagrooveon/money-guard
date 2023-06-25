52c4d6cd417fba991507a22b8499afbc0b04324b
Iryna <irinkasatanovskaya@gmail.com>
2023-06-25 19:20:54 +0300

    Update Dashboard.jsx

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import Currency from 'components/Currency/Currency';
 import { Header } from 'components/Header/Header';
 import css from './Dashboard.module.css';
+import { useMediaQuery } from 'react-responsive';
 
 const Dashboard = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767.98px' });
+
   return (
     <div className={css.dashboard}>
       <Header />
