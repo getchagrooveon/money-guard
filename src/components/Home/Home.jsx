ed15b143ecddf08480ddae6f4802933825a12701
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-06-26 12:17:19 +0300

    container edit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Transactions from '../../pages/Transactions/Transactions';
+import css from './Home.module.css';
+import EditTransactions from 'components/EditTransactions/EditTransactions';
+import { useMediaQuery } from 'react-responsive';
+import { Balance } from 'components/Balance/Balance';
+import { useSelector } from 'react-redux';
+import { selectTransaction } from 'redux/global/selectors';
+
+const Home = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const isModalEditShow = useSelector(selectTransaction) ? true : false;
+  return (
+    <div className={css.homeTab}>
+      <div>
+        {isMobile && <Balance />}
+        <Transactions />
+        {isModalEditShow && <EditTransactions />}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
