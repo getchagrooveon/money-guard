3cfb9e662e2a94de1ec613b4e24dc7ec56f69006
Iryna <irinkasatanovskaya@gmail.com>
2023-06-26 18:15:17 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 import { NavLink } from 'react-router-dom/dist';
 import { IconExit } from '../Icons/IconExit';
 import { LogoSVG } from '../Icons/LogoSVG';
-import css from './Header.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from 'redux/auth/operations';
-import { useState } from 'react';
 import { ModalLogOut } from 'components/ModalLogOut/ModalLogOut';
+import { selectShowLogout } from 'redux/global/selectors';
+import { closeLogoutModal, showLogoutModal } from 'redux/global/slice';
+import css from './Header.module.css';
 
 export const Header = () => {
-  const [modal, setModal] = useState(false);
+  const isOpenLogout = useSelector(selectShowLogout);
   const dispatch = useDispatch();
   const userName = useSelector(state => state.auth.user.username);
 
   const handleLogOut = () => {
     dispatch(logoutUser());
-    console.log('logOut');
   };
 
   const openModal = () => {
-    setModal(true);
-    console.log('open modal log out');
+    dispatch(showLogoutModal());
   };
 
   const closeModal = () => {
-    setModal(false);
-    console.log('modal log out close');
+    dispatch(closeLogoutModal());
   };
 
   return (
@@ -46,7 +44,9 @@ export const Header = () => {
           </div>
         </div>
       </header>
-      {modal && <ModalLogOut closeModal={closeModal} logout={handleLogOut} />}
+      {isOpenLogout && (
+        <ModalLogOut closeModal={closeModal} logout={handleLogOut} />
+      )}
     </>
   );
 };
