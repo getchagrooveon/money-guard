456b1cec4cbec696d6d8009547b005d79c3fd238
ginkosoul <ginkosoul@gmail.com>
2023-06-26 10:35:12 +0300

    unfinished work with bugs

--- src/components/PublicRoutes/PublicRoutes.jsx
+++ src/components/PublicRoutes/PublicRoutes.jsx
@@ -5,7 +5,7 @@ import { selectIsLoggedIn } from 'redux/auth/selectors';
 
 const PublicRoutes = () => {
   const isAuth = useSelector(selectIsLoggedIn);
-  return !isAuth ? <div>{<Outlet />}</div> : <Navigate to="/dashboard" />;
+  return !isAuth ? <div>{<Outlet />}</div> : <Navigate to="/home" />;
 };
 
 export default PublicRoutes;
