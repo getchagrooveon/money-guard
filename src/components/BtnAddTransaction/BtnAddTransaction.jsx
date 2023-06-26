af316d35d724b91536cec2553d2f21e46218ae72
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-06-26 18:29:41 +0300

    add btnAdd

--- src/components/BtnAddTransaction/BtnAddTransaction.jsx
+++ src/components/BtnAddTransaction/BtnAddTransaction.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectShowAddModal } from 'redux/global/selectors';
 import { closeAddModal, showAddModal } from 'redux/global/slice';
+import css from './BtnAddTransaction.module.css';
+
+const svgPlus = (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 0V20" stroke="white" strokeWidth="2" />
+    <path d="M0 10L20 10" stroke="white" strokeWidth="2" />
+  </svg>
+);
 
 export const BtnAddTransaction = () => {
   const isAddTransaction = useSelector(selectShowAddModal);
@@ -19,8 +33,13 @@ export const BtnAddTransaction = () => {
 
   return (
     <>
-      <button type="button" onClick={handelClick}>
-        AddTransaction
+      <button
+        type="button"
+        onClick={handelClick}
+        className={css.btnAdd}
+        style={{ borderRadius: '50%' }}
+      >
+        {svgPlus}
       </button>
       {isAddTransaction && <AddTransaction closeModal={closeModal} />}
     </>
