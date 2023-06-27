df9ec40580600cd4e23083e427691cc7544185b4
getchagrooveon <eugene.osipov.1989@gmail.com>
2023-06-27 19:53:24 +0300

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import css from './Balance.module.css';
 import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
+import { formatMoney } from 'utils/formatMoney';
 
 export const Balance = () => {
   const isDesktop = useMediaQuery({ query: '(min-width:1280px)' });
 
   const balance = useSelector(state => state.auth.user.balance);
+  console.log(formatMoney(balance));
 
   return (
     <>
@@ -14,7 +16,7 @@ export const Balance = () => {
         <div className={css.balanceBox}>
           <p className={css.balanceTitle}>Your balance</p>
           <p className={css.balance}>
-            <span className={css.uah}>&#8372;</span> {balance.toFixed(2)}
+            <span className={css.uah}>&#8372;</span> {formatMoney(balance)}
           </p>
         </div>
       )}
@@ -22,7 +24,7 @@ export const Balance = () => {
         <div className={css.balanceBox}>
           <p className={css.balanceTitle}>Your balance</p>
           <p className={css.balance}>
-            <span className={css.uah}>&#8372;</span> {balance.toFixed(2)}
+            <span className={css.uah}>&#8372;</span> {formatMoney(balance)}
           </p>
         </div>
       )}
