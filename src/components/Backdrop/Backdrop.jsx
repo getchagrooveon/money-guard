e29086321ee685a4d605e82a775af2b30e7f7c76
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-06-27 18:56:26 +0300

    fix

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -1,7 +1,6 @@
 import css from './Backdrop.module.css';
 
 export const Backdrop = ({ onClick, children }) => {
-  console.log('children', children);
   return (
     <div onClick={onClick} className={css.backdrop}>
       {children}
